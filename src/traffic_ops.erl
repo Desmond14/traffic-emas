eaded583b3364731d061c027c3dfe0bf98f4b047
Jan Stypka <janstypka@gmail.com>
2015-04-05 18:54:59 +0200

    Introduce real non binary operators

--- src/traffic_ops.erl
+++ src/traffic_ops.erl
@@ -7,52 +7,39 @@
 -include_lib("emas/include/emas.hrl").
 
 -type sim_params() :: emas:sim_params().
--type solution() :: emas:solution(binary()).
+-type solution() :: emas:solution([tuple()]).
 
 
 %% @doc Generates a random solution, a list of 4-bit tuples
 -spec solution(sim_params()) -> solution().
 solution(#sim_params{problem_size = ProblemSize}) ->
-    S = [{random_bit(), random_bit(), random_bit(), random_bit()}
-         || _ <- lists:seq(1, ProblemSize)],
-    erlang:term_to_binary(S).
+    [{random_bit(), random_bit(), random_bit(), random_bit()}
+     || _ <- lists:seq(1, ProblemSize)].
 
 
 %% @doc Evaluates a given solution and returns a fitness value
 -spec evaluation(solution(), sim_params()) -> float().
-evaluation(Binary, #sim_params{extra = Data}) ->
-    Solution = erlang:binary_to_term(Binary),
+evaluation(Solution, #sim_params{extra = Data}) ->
     Fitness = evaluation:evaluate_solution(Solution, Data),
     float(Fitness).
 
 
 %% @doc Crossover recombination in a random point
 -spec recombination(solution(), solution(), sim_params()) -> {solution(), solution()}.
-recombination(B1, B2, #sim_params{problem_size = ProblemSize}) ->
-    {S1, S2} = {erlang:binary_to_term(B1), erlang:binary_to_term(B2)},
+recombination(S1, S2, #sim_params{problem_size = ProblemSize}) ->
     CutPoint = random:uniform(ProblemSize),
     {S1a, S1b} = lists:split(CutPoint, S1),
     {S2a, S2b} = lists:split(CutPoint, S2),
-    {erlang:term_to_binary(S1a ++ S2b), erlang:term_to_binary(S2a ++ S1b)}.
+    {S1a ++ S2b, S2a ++ S1b}.
 
 
 %% @doc Mutates (flips) genes at random indexes
 -spec mutation(solution(), sim_params()) -> solution().
-mutation(B, SP = #sim_params{mutation_rate = MutRate}) ->
-    Solution = erlang:binary_to_term(B),
-    %%     Mutated = [case random:uniform() < MutRate of
-    %%                    true -> flip_gene(Gene);
-    %%                    false -> Gene
-    %%                end || Gene <- Solution],
-    NrGenesMutated = mas_misc_util:average_number(MutRate, Solution),
-    Indexes = [random:uniform(length(Solution))
-               || _ <- lists:seq(1, NrGenesMutated)], % indexes may be duplicated
-    Mutated = mutate_genes(Solution,
-                           lists:usort(Indexes),
-                           1,
-                           [],
-                           SP), % usort removes duplicates
-    erlang:term_to_binary(Mutated).
+mutation(Solution, #sim_params{mutation_rate = MutRate}) ->
+    [case random:uniform() < MutRate of
+         true -> flip_gene(Gene);
+         false -> Gene
+     end || Gene <- Solution].
 
 
 %% @doc Loads the data that will be optimized
@@ -67,20 +54,6 @@ config() ->
 -spec random_bit() -> 0 | 1.
 random_bit() ->
     random:uniform(2) - 1.
-
-
-%% @doc Flips genes under given indexes
-mutate_genes(RestOfSolution, [], _, Acc, _SP) ->
-    lists:reverse(Acc, RestOfSolution);
-
-mutate_genes([], [_|_], _, _, _) ->
-    erlang:error(tooManyIndexes);
-
-mutate_genes([Gene|Solution], [I|Indexes], I, Acc, SP) ->
-    mutate_genes(Solution, Indexes, I+1, [flip_gene(Gene) | Acc], SP);
-
-mutate_genes([Gene|Solution], [I|Indexes], Inc, Acc, SP) ->
-    mutate_genes(Solution, [I|Indexes], Inc+1, [Gene|Acc], SP).
 
 
 %% @doc Flips the whole gene
