f9fc34fc104e9fd648af190490dce49352e3dbb6
slakomy <s.lakomy@gmail.com>
2016-05-28 16:16:00 +0200

    Initial population with symetric lights

--- src/traffic_ops.erl
+++ src/traffic_ops.erl
@@ -22,25 +22,25 @@ solution(#sim_params{problem_size = ProblemSize}) ->
 %% @doc Evaluates a given solution and returns a fitness value
 -spec evaluation(solution(), sim_params()) -> float().
 evaluation(Solution, #sim_params{extra = Input}) ->
-    {Data, SaveSimulationCourse} = Input,
-    Fitness = evaluation_multilane:evaluate_solution(Solution, Data, SaveSimulationCourse),
-    float(Fitness).
+  {Data, SaveSimulationCourse} = Input,
+  Fitness = evaluation_multilane:evaluate_solution(Solution, Data, SaveSimulationCourse),
+  float(Fitness).
 
 %% @doc Crossover recombination in a random point
 -spec recombination(solution(), solution(), sim_params()) -> {solution(), solution()}.
 recombination(S1, S2, #sim_params{problem_size = ProblemSize}) ->
-    CutPoint = random:uniform(ProblemSize),
-    {S1a, S1b} = lists:split(CutPoint, S1),
-    {S2a, S2b} = lists:split(CutPoint, S2),
-    {S1a ++ S2b, S2a ++ S1b}.
+  CutPoint = random:uniform(ProblemSize),
+  {S1a, S1b} = lists:split(CutPoint, S1),
+  {S2a, S2b} = lists:split(CutPoint, S2),
+  {S1a ++ S2b, S2a ++ S1b}.
 
 %% @doc Mutates genes at random indexes
 -spec mutation(solution(), sim_params()) -> solution().
 mutation(Solution, #sim_params{mutation_rate = MutRate, mutation_range = MutRan}) ->
-    [case random:uniform() < MutRate of
-         true -> mutate_gene(Gene, MutRan);
-         false -> Gene
-     end || Gene <- Solution].
+  [case random:uniform() < MutRate of
+     true -> mutate_gene(Gene, MutRan);
+     false -> Gene
+   end || Gene <- Solution].
 
 
 %% @doc Loads the data for which lights will be optimized
@@ -52,15 +52,10 @@ config() ->
 %% Internal functions
 %% =============================================================================
 
--spec random_trit() -> trit().
-random_trit() ->
-    random:uniform(3) - 1.
-
 -spec mutate_gene(lights(), float()) -> lights().
 mutate_gene(Lights, MutationRange) ->
   NodesToMutate = [NodeId || NodeId <- maps:keys(Lights), random:uniform() < MutationRange],
   change_light(NodesToMutate, Lights).
-
 
 -spec change_light([node_id()], lights()) -> lights().
 change_light([], Lights) ->
@@ -82,34 +77,49 @@ change_light([NodeId | Rest], Lights) ->
 
 -spec generate_solution(intersection(), pos_integer()) -> solution().
 generate_solution(Intersection, ProblemSize) ->
-  InitialLightValue = random_trit(),
-  InitialLightDuration = draw_light_duration(ProblemSize),
+  InitialLightValue = draw_next_light(?BOTH_RED),
   SemaphoreIds = intersection:extract_ids(intersection:get_semaphores(Intersection)),
-  PartialSolution = [create_lights(InitialLightValue, SemaphoreIds) || _ <- lists:seq(1, InitialLightDuration)],
-  generate_solution(PartialSolution, SemaphoreIds, ProblemSize-InitialLightDuration, InitialLightValue).
+  generate_solution(InitialLightValue, SemaphoreIds, ProblemSize, []).
 
--spec generate_solution(solution(), [node_id()], non_neg_integer(), trit()) -> solution().
-generate_solution(Solution, _SemaphoreIds, 0, _PreviousLightValue) ->
-  Solution;
-
-generate_solution(PartialSolution, SemaphoreIds, LeftProblemSize, PreviousLight) ->
-  LightValue = draw_next_light(PreviousLight),
-  LightDuration = draw_light_duration(LeftProblemSize),
-  NextSolutionPart = [create_lights(LightValue, SemaphoreIds) || _ <- lists:seq(1, LightDuration)],
-  generate_solution(PartialSolution ++ NextSolutionPart, SemaphoreIds, LeftProblemSize-LightDuration, LightValue).
-
--spec create_lights(trit(), [node_id()]) -> lights().
-create_lights(LightValue, SemaphoreIds) ->
-  create_lights(SemaphoreIds, maps:new(), LightValue).
-
-create_lights([], Lights, _LightValue) ->
+-spec generate_solution(trit(), [node_id()], non_neg_integer(), lights()) -> lights().
+generate_solution(_InitialLightValue, _SemaphoreIds, 0, Lights) ->
   Lights;
 
-create_lights([SemaphoreId | Rest], Lights, LightValue) ->
-  create_lights(Rest, maps:put(SemaphoreId, LightValue, Lights), LightValue).
+generate_solution(InitialLightValue, SemaphoreIds, ProblemSize, Lights) ->
+  LightCycle = create_lights_cycle(InitialLightValue, draw_light_duration(ProblemSize), SemaphoreIds, ProblemSize),
+  generate_solution(InitialLightValue, SemaphoreIds, ProblemSize-length(LightCycle), Lights ++ LightCycle).
+
+-spec create_lights_cycle(trit(), non_neg_integer(), [node_id()], non_neg_integer()) -> lights().
+create_lights_cycle(InitialLightValue, SingleLightDuration, SemaphoreIds, TotalLightsDuration) ->
+  lists:sublist(
+    create_lights(InitialLightValue, SingleLightDuration, SemaphoreIds)
+    ++ create_lights(?BOTH_RED, ?SAME_LIGHT_AT_LEAST_FOR, SemaphoreIds)
+      ++ create_lights(opposite_light(InitialLightValue), SingleLightDuration, SemaphoreIds)
+      ++ create_lights(?BOTH_RED, ?SAME_LIGHT_AT_LEAST_FOR, SemaphoreIds),
+    TotalLightsDuration).
+
+%%Possible optimization by creating lights configuration only one?
+-spec create_lights(trit(), pos_integer(), [node_id()]) -> lights().
+create_lights(LightValue, LightDuration, SemaphoreIds) ->
+  [create_lights_configuration(LightValue, SemaphoreIds) || _ <- lists:seq(1, LightDuration)].
+
+create_lights_configuration(LightValue, SemaphoreIds) ->
+  create_lights_configuration(LightValue, SemaphoreIds, maps:new()).
+
+create_lights_configuration(_LightValue, [], Result) ->
+  Result;
+
+create_lights_configuration(LightValue, [NodeId | Rest], Result) ->
+  create_lights_configuration(LightValue, Rest, maps:put(NodeId, LightValue, Result)).
 
 draw_light_duration(ProblemSize) ->
   min(ProblemSize, random:uniform(?SAME_LIGHT_AT_MOST_FOR-?SAME_LIGHT_AT_LEAST_FOR+1)+?SAME_LIGHT_AT_LEAST_FOR-1).
+
+opposite_light(Light) ->
+  case Light of
+    ?GREEN_FOR_HIGHER_ID -> ?GREEN_FOR_LOWER_ID;
+    ?GREEN_FOR_LOWER_ID -> ?GREEN_FOR_HIGHER_ID
+  end.
 
 draw_next_light(PreviousLight) ->
   case PreviousLight of
