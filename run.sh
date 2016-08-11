0c134aa4727c241d495720f723227d22403fe122
slakomy <s.lakomy@gmail.com>
2016-08-11 20:18:55 +0200

    Added 2x3 intersection, added script for evaluating fitness function

--- run.sh
+++ run.sh
@@ -1,13 +1,25 @@
-InitialTime=5000
-MaxTime=5000
-TimeStep=10000
+#!/bin/bash
+#PBS -q plgrid
+#PBS -l walltime=08:00:00
+#PBS -l nodes=1:ppn=12
+module load plgrid/apps/erlang
+
 ProblemSize=20
-Iterations=5
+Iterations=30
 MutationRate=0.20
 MutationRange=0.20
 Model=mas_skel
 
-for ((t=$InitialTime; t<=$MaxTime; t+=$TimeStep)); do
+cd /people/plgslakomy/traffic-emas
+make
+
+cp priv/cars10.input priv/input.cars
+
+echo "cars10.input" >> result.txt
+
+echo $(date '+%Y %b %d %H:%M')
+
+for t in 1000 2000 3000 4000 6000 8000 10000 12000 16000 20000 24000; do
     echo -n $t >> result.txt
     echo -n $'\t' >> result.txt
     for ((iter=0; iter<$Iterations; iter++)); do
@@ -16,3 +28,35 @@ for ((t=$InitialTime; t<=$MaxTime; t+=$TimeStep)); do
      done
      echo "" >> result.txt
 done
+
+echo $(date '+%Y %b %d %H:%M')
+
+cp priv/cars60.input priv/input.cars
+echo "cars60.input" >> result.txt
+
+for t in 1000 2000 3000 4000 6000 8000 10000 12000 16000 20000 24000; do
+    echo -n $t >> result.txt
+    echo -n $'\t' >> result.txt
+    for ((iter=0; iter<$Iterations; iter++)); do
+        ./emas --time $t --genetic_ops traffic_ops --model $Model --problem_size $ProblemSize --mutation_rate $MutationRate --mutation_range $MutationRange
+        echo -n $'\t' >> result.txt
+     done
+     echo "" >> result.txt
+done
+
+echo $(date '+%Y %b %d %H:%M')
+
+cp priv/cars30.input priv/input.cars
+echo "cars30.input" >> result.txt
+
+for t in 1000 2000 3000 4000 6000 8000 10000 12000 16000 20000 24000; do
+    echo -n $t >> result.txt
+    echo -n $'\t' >> result.txt
+    for ((iter=0; iter<$Iterations; iter++)); do
+        ./emas --time $t --genetic_ops traffic_ops --model $Model --problem_size $ProblemSize --mutation_rate $MutationRate --mutation_range $MutationRange
+        echo -n $'\t' >> result.txt
+     done
+     echo "" >> result.txt
+done
+
+echo $(date '+%Y %b %d %H:%M')
